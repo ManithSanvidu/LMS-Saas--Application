5497f7db27257a5c3d59420ef20d0593b16283b5
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-06-01 20:48:31 +0530

    Update page.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,1 +1,11 @@
+"use client";
 
+import { SignUp } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <main className='flex items-center justify-center min-h-screen'>
+      <SignUp />
+    </main>
+  );
+}
